8c83cb5e22ec572ef8ef07165a7782f4cd6ab4cc
Gaurav Singh <101568403+GauravSinghh@users.noreply.github.com>
2022-10-12 18:17:45 +0530

    Delete EmployeeApp/EmployeeApp directory

--- EmployeeApp/EmployeeApp/src/main/java/com/masai/repositary/AddressDao.java
+++
@@ -1,9 +0,0 @@
-package com.masai.repositary;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.masai.entity.Address;
-
-public interface AddressDao extends JpaRepository<Address, Integer>{
-
-}
--- EmployeeApp/EmployeeApp/src/main/java/com/masai/repositary/EmployeeDao.java
+++
@@ -1,9 +0,0 @@
-package com.masai.repositary;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.masai.entity.Employee;
-
-public interface EmployeeDao extends JpaRepository<Employee	, Integer>{
-
-}
